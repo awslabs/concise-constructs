ca8444a76d10623e6a784b6a6f3b3f2e7ee13254
Harry <harrysolovay@gmail.com>
2020-12-22 14:17:52 -0500

    docs: get rid of comments from rest-api example

--- examples/rest-api/index.ts
+++ examples/rest-api/index.ts
@@ -4,12 +4,9 @@ import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import path from "path";
 
-// create the `AssetCode` instance for later use in any Lambda function(s)
 const code = new lambda.AssetCode(path.resolve(__dirname, "lambda", "dist"));
 
-// define the `Stack`
 const Stack = C(cdk.Stack, (def, environment?: Record<string, string>) => {
-  // define a handler, which will use the `helloConciseConstructsHandler` export
   const handler = def`handler`(lambda.Function, {
     code,
     handler: "index.helloConciseConstructsHandler",
@@ -21,10 +18,8 @@ const Stack = C(cdk.Stack, (def, environment?: Record<string, string>) => {
   def`api`(apigw.LambdaRestApi, {handler});
 });
 
-// define the `App`
 const App = C(cdk.App, (def) => {
   def`stack`(Stack, {SOME_ENV_VAR: "Lorem ipsum dolor!"});
 });
 
-// synthesize to the file system (to-be deployed!)
 new App().synth();
