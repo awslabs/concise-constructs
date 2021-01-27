01996b3cc8d84b9cc1f3a5ef5666277179d2b949
Harry Solovay <harrysolovay@gmail.com>
2021-01-26 20:08:16 -0800

    test: IsRoot on cdk8s App and Chart

--- src/ctor.test.ts
+++ src/ctor.test.ts
@@ -1,6 +1,7 @@
 import {assert, IsExact} from "conditional-type-checks";
 import {Ctor} from "./ctor";
 import type * as cdk from "@aws-cdk/core";
+import type * as cdk8s from "cdk8s";
 
 describe("Ctor", () => {
   it("IsRoot Type", () => {
@@ -8,5 +9,7 @@ describe("Ctor", () => {
     assert<IsExact<Ctor.IsRoot<typeof cdk.App>, true>>(true);
     assert<IsExact<Ctor.IsRoot<typeof cdk.Stack>, false>>(true);
     assert<IsExact<Ctor.IsRoot<typeof cdk.Construct>, false>>(true);
+    assert<IsExact<Ctor.IsRoot<typeof cdk8s.App>, true>>(true);
+    assert<IsExact<Ctor.IsRoot<typeof cdk8s.Chart>, false>>(true);
   });
 });
